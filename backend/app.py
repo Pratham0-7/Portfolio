c30341d0cc7bb5922e9d8502792a9a88cfdde1fa
Pratham <pratham07603@gmail.com>
2024-09-11 11:14:24 +0530

    Updated app.py with MongoDB URI and database changes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import logging
+import ssl
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -12,7 +13,7 @@ app = Flask(__name__)
 CORS(app, origins=["https://makemyweb.netlify.app"])
 
 MONGO_URI = os.getenv('MONGO_URI')
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, ssl=False)
 db = client[os.getenv('DB_NAME')]
 contact_collection = db[os.getenv('COLLECTION_NAME')]
 
@@ -31,17 +32,20 @@ def test_db():
         return str(e)
 
 
-@app.route('/contact', methods=['POST'])
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    try:
-        data = request.json
-        result = contact_collection.insert_one(data)
-        response_data = serialize_document(data)
-        response_data['_id'] = str(result.inserted_id)
-        return jsonify({"message": "Data received and saved!", "data": response_data})
-    except Exception as e:
-        logging.error(f"Error occurred: {e}")
-        return jsonify({"error": "Internal Server Error"}), 500
+    if request.method == 'POST':
+        try:
+            data = request.json
+            result = contact_collection.insert_one(data)
+            response_data = serialize_document(data)
+            response_data['_id'] = str(result.inserted_id)
+            return jsonify({"message": "Data received and saved!", "data": response_data})
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            return jsonify({"error": "Internal Server Error"}), 500
+    else:
+        return jsonify({"message": "GET method not allowed for this endpoint."}), 405
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
